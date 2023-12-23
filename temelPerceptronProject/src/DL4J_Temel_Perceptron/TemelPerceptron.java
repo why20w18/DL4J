50206feb54ffcd79cf59eccd4c2778ae91e82265
why20w18 <enco3162@gmail.com>
2023-12-23 20:51:25 +0300

    gecici aktivasyon fonksiyonu olusturuldu weight random atamalar yapildi

--- temelPerceptronProject/src/DL4J_Temel_Perceptron/TemelPerceptron.java
+++ temelPerceptronProject/src/DL4J_Temel_Perceptron/TemelPerceptron.java
@@ -1,10 +1,12 @@
 package DL4J_Temel_Perceptron;
 
+import java.util.Random;
+
 
 public class TemelPerceptron {
     //weightleri tanimlarim
     double w1,w2;
-    
+    TemelPerceptron referans = new TemelPerceptron();
     
     //agirlik ve inputlarin carpilacagi fonksiyon
     public double toplamaFonksiyon(double x1 , double x2){
@@ -13,6 +15,25 @@ public class TemelPerceptron {
         return toplam;
     }
     
+    //baslangicta weightlere random deger atama
+    Random rand = new Random();
+    public void randomAtamaWeight(){
+        //0-1 arasi deger dondurur -1 ve 1 cekecegiz
+        w1 = (rand.nextDouble()-0.5)*2;
+        w2 = (rand.nextDouble()-0.5)*2;
+    }
+    
+    //gecici aktivasyon fonksiyonu
+    public double tempAktivasyonFonksiyon(TemelPerceptron referans){
+        
+        double toplam = referans.toplamaFonksiyon(w1, w2);
+        
+        if(toplam > 1)
+            return 1;
+        else
+            return -1;
+        
+    }
     
     
 }
