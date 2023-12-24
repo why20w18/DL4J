e11f7a55ceb559457c2afa445b87195c4b17fe71
why20w18 <enco3162@gmail.com>
2023-12-24 13:10:27 +0300

    referans ortadan kaldirildi

--- temelPerceptronProject/src/DL4J_Temel_Perceptron/TemelPerceptron.java
+++ temelPerceptronProject/src/DL4J_Temel_Perceptron/TemelPerceptron.java
@@ -6,27 +6,28 @@ import java.util.Random;
 public class TemelPerceptron {
     //weightleri tanimlarim
     double w1,w2;
-    TemelPerceptron referans = new TemelPerceptron();
-    
-    //agirlik ve inputlarin carpilacagi fonksiyon
-    public double toplamaFonksiyon(double x1 , double x2){
-        
-        double toplam = ((w1 * x1) + (w2 * x2));
-        return toplam;
-    }
-    
-    //baslangicta weightlere random deger atama
+
+     //baslangicta weightlere random deger atama
     Random rand = new Random();
     public void randomAtamaWeight(){
-        //0-1 arasi deger dondurur -1 ve 1 cekecegiz
+        //0-1 arasi deger dondurur -1 ve 1 cekecegiz kullanılcak aktivasyon fonksiyonuna bagli
         w1 = (rand.nextDouble()-0.5)*2;
         w2 = (rand.nextDouble()-0.5)*2;
     }
     
+    
+    //agirlik ve inputlarin carpilacagi fonksiyon
+    public double toplamaFonksiyon(double x1 , double x2){
+        randomAtamaWeight();
+        double toplam = ((w1 * x1) + (w2 * x2));
+        return toplam;
+    }
+    
+   
     //gecici aktivasyon fonksiyonu
-    public double tempAktivasyonFonksiyon(TemelPerceptron referans){
+    public double tempAktivasyonFonksiyon(double x1 , double x2){
         
-        double toplam = referans.toplamaFonksiyon(w1, w2);
+        double toplam = toplamaFonksiyon(x1, x2);
         
         if(toplam > 1)
             return 1;
