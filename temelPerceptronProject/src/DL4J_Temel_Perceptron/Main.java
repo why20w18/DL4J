3619c7eb22c962c9df92f925e7085fd083657ffa
why20w18 <enco3162@gmail.com>
2023-12-24 13:20:39 +0300

    Sonsuz dongude w ve output test edildi

--- temelPerceptronProject/src/DL4J_Temel_Perceptron/Main.java
+++ temelPerceptronProject/src/DL4J_Temel_Perceptron/Main.java
@@ -5,10 +5,20 @@ import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
         TemelPerceptron perceptron = new TemelPerceptron();
         
-        System.out.println(perceptron.tempAktivasyonFonksiyon(24, 15));
+        double x1 = 0,x2 = 0;
+        int i = 1;
+        while(true){
+        //System.out.print("x1:");     x1 = scanner.nextDouble();
+        //System.out.print("x2:");     x2 = scanner.nextDouble();
+        System.out.println("\n"+i+"- Output Layer:"+perceptron.tempAktivasyonFonksiyon(24, 15));   
+        i++;
+        }
+        
+        
         
     }
     
